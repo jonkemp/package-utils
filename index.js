834e2704af0783f5d4c82171a7862d969d0272f5
Jonathan Kemp <kempdogg@gmail.com>
2020-03-27 23:48:44 -0400

    Split out modules.

--- index.js
+++ index.js
@@ -1,52 +1,26 @@
-const shallowProperty = key => obj => obj == null ? void 0 : obj[key];
-
-const MAX_ARRAY_INDEX = 2 ** 53 - 1;
-
-const getLength = shallowProperty('length');
-
-const isArrayLike = (collection) => {
-	const length = getLength(collection);
-
-	return typeof length == 'number' && length >= 0 && length <= MAX_ARRAY_INDEX;
-};
-
-const isFunction = obj => toString.call(obj) === '[object Function]';
+const {
+	getLength,
+	isArrayLike,
+	isFunction,
+	isObject,
+	isArguments,
+	identity,
+	getKeys,
+	property,
+	matcher,
+	isMatch,
+	optimizeCb,
+	cb
+} = require('./lib');
+const forEach = require('./lib/for-each');
+const flatten = require('./lib/flatten');
+const map = require('./lib/map');
 
 const isNumber = obj => toString.call(obj) === '[object Number]';
-
-const isArguments = obj => toString.call(obj) === '[object Arguments]';
 
 const isString = obj => toString.call(obj) === '[object String]';
 
 const isUndefined = obj => obj === void 0;
-
-const isObject = obj => {
-	const type = typeof obj;
-
-	return type === 'function' || type === 'object' && !!obj;
-};
-
-const getKeys = (obj) => {
-	if (!isObject(obj)) return [];
-
-	return Object.keys(obj);
-};
-
-const isMatch = (object, attrs) => {
-	const keys = getKeys(attrs);
-	const {length} = keys;
-
-	if (object == null) return !length;
-	const obj = Object(object);
-
-	for (let i = 0; i < length; i++) {
-		const key = keys[i];
-
-		if (attrs[key] !== obj[key] || !(key in obj)) return false;
-	}
-
-	return true;
-};
 
 const has = (obj, path) => obj != null && Object.prototype.hasOwnProperty.call(obj, path);
 
@@ -67,8 +41,6 @@ const hasProperty = (obj, path) => {
 
 	return !!length;
 };
-
-const identity = value => value;
 
 const constant = value => () => value;
 
@@ -104,61 +76,6 @@ const toPairs = (obj) => {
 	}
 
 	return pairs;
-};
-
-const optimizeCb = (func, context, argCount) => {
-	if (context === void 0) return func;
-	switch (argCount == null ? 3 : argCount) {
-		case 1: return value => func.call(context, value);
-			// The 2-argument case is omitted because we’re not using it.
-		case 3: return (value, index, collection) => func.call(context, value, index, collection);
-		case 4: return (accumulator, value, index, collection) => func.call(context, accumulator, value, index, collection);
-	}
-
-	return (...args) => func.apply(context, args);
-};
-
-const matcher = attrs => {
-	attrs = Object.assign({}, attrs);
-
-	return obj => isMatch(obj, attrs);
-};
-
-const deepGet = (obj, path) => {
-	const { length } = path;
-
-	for (let i = 0; i < length; i++) {
-		if (obj == null) return void 0;
-		obj = obj[path[i]];
-	}
-
-	return length ? obj : void 0;
-};
-
-const property = path => {
-	if (!Array.isArray(path)) {
-		return shallowProperty(path);
-	}
-
-	return obj => deepGet(obj, path);
-};
-
-const baseIteratee = (value, context, argCount) => {
-	if (value == null) return identity;
-	if (isFunction(value)) return optimizeCb(value, context, argCount);
-	if (isObject(value) && !Array.isArray(value)) return matcher(value);
-
-	return property(value);
-};
-
-let iteratee;
-
-const exportIteratee = iteratee = (value, context) => baseIteratee(value, context, Infinity);
-
-const cb = (value, context, argCount) => {
-	if (iteratee !== exportIteratee) return iteratee(value, context);
-
-	return baseIteratee(value, context, argCount);
 };
 
 const noop = () => {};
@@ -209,40 +126,6 @@ const findKey = (obj, predicate, context) => {
 	}
 };
 
-const forEach = (obj, iteratee, context) => {
-	iteratee = optimizeCb(iteratee, context);
-	if (isArrayLike(obj)) {
-		let i = 0;
-
-		for (const item of obj) {
-			iteratee(item, i++, obj);
-		}
-	} else {
-		const keys = getKeys(obj);
-
-		for (const key of keys) {
-			iteratee(obj[key], key, obj);
-		}
-	}
-
-	return obj;
-};
-
-const map = (obj, iteratee, context) => {
-	iteratee = cb(iteratee, context);
-	const keys = !isArrayLike(obj) && getKeys(obj);
-	const { length } = keys || obj;
-	const results = Array(length);
-
-	for (let index = 0; index < length; index++) {
-		const currentKey = keys ? keys[index] : index;
-
-		results[index] = iteratee(obj[currentKey], currentKey, obj);
-	}
-
-	return results;
-};
-
 const find = (obj, predicate, context) => {
 	const keyFinder = isArrayLike(obj) ? findIndex : findKey;
 	const key = keyFinder(obj, predicate, context);
@@ -281,30 +164,6 @@ const first = (array, n, guard) => {
 
 	return initial(array, array.length - n);
 };
-
-const baseFlatten = (input, shallow, strict, output = []) => {
-	let idx = output.length;
-
-	forEach(input, value => {
-		if (isArrayLike(value) && (Array.isArray(value) || isArguments(value))) {
-			if (shallow) {
-				let j = 0;
-				const len = value.length;
-
-				while (j < len) output[idx++] = value[j++];
-			} else {
-				baseFlatten(value, shallow, strict, output);
-				idx = output.length;
-			}
-		} else if (!strict) {
-			output[idx++] = value;
-		}
-	});
-
-	return output;
-};
-
-const flatten = (array, shallow) => baseFlatten(array, shallow, false);
 
 module.exports = {
 	getLength,
